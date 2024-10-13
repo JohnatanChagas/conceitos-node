4f35ccbfcb5dea16975230c8ef13ecfeb8bfe10b
JohnatanChagas <johnatan_pereira@hotmail.com>
2024-10-13 11:15:05 -0300

    estudos de node com prisma e express

---
+++ server.js
@@ -0,0 +1,66 @@
+//  Antigo
+// const express = require("express")
+
+// Novo
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.status(200).json(users);
+});
+
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
+
+  res.status(201).json(user);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
+
+  res.status(200).json(user);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
+});
+
+app.listen(3000);
+
+/* 
+chave de acesso mongoDB - johnatan_pereira@hotmail.com
+ nome: johnatanchagas 
+ senha:   wwpsDPMLHtZjsZsA
+
+
+
+ mongodb+srv://johnatanchagas:<db_password>@users.xqsmv.mongodb.net/?retryWrites=true&w=majority&appName=Users
+*/
